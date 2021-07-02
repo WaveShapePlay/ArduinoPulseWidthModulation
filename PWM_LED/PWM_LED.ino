8448e1c4677320e9a7cd80814ff774f157c02d6c
WaveShapePlay <WaveShapePlay@MacBook-Pro.fios-router.home>
2021-07-02 12:12:58 -0400

    Added first blink loop for LED

--- PWM_LED/PWM_LED.ino
+++ PWM_LED/PWM_LED.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
 
+int LED_Pin_3 = 3;
+
+void setup() {
+  pinMode(LED_Pin_3, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //int i = 0;
+  for(int i=0; i<225;i++){
+    analogWrite(LED_Pin_3,i);
+    delay(10);                      // milli seconds
+  }
+  
 }
