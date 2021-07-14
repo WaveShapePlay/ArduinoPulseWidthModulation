42a55098e8fca9a9ef265706a7914768bd052766
Robert John <WaveShapePlay@gmail.com>
2021-07-14 06:00:56 -0400

    Added scale bar.

--- PWM_GUI.py
+++ PWM_GUI.py
@@ -23,7 +23,7 @@ def turnOff_pwm():
 
 def entry_pwm():
     inputMode()
-    value = entry_pwm.get()
+    value = userDutyScale.get()
     ser.write(value.encode())
     
 # creating tkinter window 
@@ -36,14 +36,21 @@ btn_On.grid(row=0, column=0)
 btn_Off = tk.Button(root, text="Turn Off", command=turnOff_pwm)
 btn_Off.grid(row=0, column=1)
 
+
 btn_entry = tk.Button(root, text="Get Entry", command=entry_pwm)
 btn_entry.grid(row=0, column=2)
 
+userDutyScale = StringVar()
+scaleBar_pwm = tk.Scale(root,from_=0,to = 255,orient=HORIZONTAL, variable = userDutyScale)
+scaleBar_pwm.grid(row=2, column = 0)
+scaleBar_Label = tk.Label(root,text="analogWrite() value")
+scaleBar_Label.grid(row=3,column =0)
+
+'''
 entry_pwm = Entry(root,width=3)
 entry_pwm.insert(0,"100")
-entry_pwmLabel = tk.Label(root,text="analogWrite() value")
-entry_pwmLabel.grid(row=2,column =0)
 entry_pwm.grid(row=2,column = 1)
+'''
 
 root.geometry("350x350")
 root.mainloop()
