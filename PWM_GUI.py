fe99ab2d58e8969de930e4392fe4d353f2ff1a1b
Robert John <WaveShapePlay@gmail.com>
2021-07-20 06:17:48 -0400

    Made PWM_Class

--- PWM_GUI.py
+++ PWM_GUI.py
@@ -7,44 +7,46 @@ import time
 commPort = 'COM4'
 ser = serial.Serial(commPort, baudrate = 9600, timeout = 1)
 
-def inputMode():
-    ser.write(b'd')
-    time.sleep(1)
-    
-def turnOn_pwm():
-    inputMode()
-    value = '255'
-    ser.write(value.encode())
-
-def turnOff_pwm():
-    inputMode()
-    value = '0'
-    ser.write(value.encode())
-
-def entry_pwm():
-    inputMode()
-    value = userDutyScale.get()
-    ser.write(value.encode())
-    
 # creating tkinter window 
 root = Tk() 
 root.title('PWM GUI')
 
-btn_On = tk.Button(root, text="Turn On", command=turnOn_pwm)
+userDutyScale = StringVar()
+userDutyScale.set('11')
+
+class PWM_mode():
+    
+    def inputMode(self):
+        ser.write(b'd')
+        time.sleep(1)
+        
+    def turnOn_pwm(self):
+        self.inputMode()
+        value = '255'
+        ser.write(value.encode())
+
+    def turnOff_pwm(self):
+        self.inputMode()
+        value = '0'
+        ser.write(value.encode())
+
+    def entry_pwm(self):
+        self.inputMode()
+        value = userDutyScale.get()
+        ser.write(value.encode())
+
+
+pwm = PWM_mode()
+
+btn_On = tk.Button(root, text="Turn On", command=pwm.turnOn_pwm)
 btn_On.grid(row=0, column=0)
 
-btn_Off = tk.Button(root, text="Turn Off", command=turnOff_pwm)
+btn_Off = tk.Button(root, text="Turn Off", command=pwm.turnOff_pwm)
 btn_Off.grid(row=0, column=1)
 
 
-btn_entry = tk.Button(root, text="Get Entry", command=entry_pwm)
+btn_entry = tk.Button(root, text="Get Entry", command=pwm.entry_pwm)
 btn_entry.grid(row=0, column=2)
-
-userDutyScale = StringVar()
-scaleBar_pwm = tk.Scale(root,from_=0,to = 255,orient=HORIZONTAL, variable = userDutyScale)
-scaleBar_pwm.grid(row=2, column = 0)
-scaleBar_Label = tk.Label(root,text="analogWrite() value")
-scaleBar_Label.grid(row=3,column =0)
 
 '''
 entry_pwm = Entry(root,width=3)
@@ -52,5 +54,9 @@ entry_pwm.insert(0,"100")
 entry_pwm.grid(row=2,column = 1)
 '''
 
+scaleBar_pwm = tk.Scale(root,from_=0,to = 255,orient=HORIZONTAL, variable = userDutyScale)
+scaleBar_pwm.grid(row=2, column = 0)
+scaleBar_Label = tk.Label(root,text="analogWrite() value")
+scaleBar_Label.grid(row=3,column =0)
 root.geometry("350x350")
 root.mainloop()
