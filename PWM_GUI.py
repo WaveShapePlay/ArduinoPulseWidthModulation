c7ab71522406c6cbccb4b1947cf219232e8bf756
Robert John <WaveShapePlay@gmail.com>
2021-07-24 06:04:04 -0400

    Clean up 3

--- PWM_GUI.py
+++ PWM_GUI.py
@@ -23,10 +23,9 @@ currntValFrame.pack(pady = padFrames)
 
 
 
-defaultDuty = '128'
+defaultDuty = '0'
 userDutyScale = StringVar()
 userDutyScale.set(defaultDuty)
-
 curretUserValue = StringVar()
 
 
@@ -77,7 +76,7 @@ btn_EntryBox.grid(row=0, column=3, pady= padyButtons)
 
 scaleBar_pwm = tk.Scale(scaleBarFrame,from_=0,to = 255,orient=HORIZONTAL, variable = userDutyScale)
 scaleBar_pwm.grid(row=0, column = 0)
-scaleBar_Label = tk.Label(scaleBarFrame,text="analogWrite() value")
+scaleBar_Label = tk.Label(scaleBarFrame,text="Slider Value")
 scaleBar_Label.grid(row=1,column =0)
 
 entry_pwm = Entry(entryFrame,width=3)
@@ -86,7 +85,7 @@ entry_pwm.grid(row=0,column = 0)
 entry_pwm_Label= tk.Label(entryFrame,text="Entry Value")
 entry_pwm_Label.grid(row=1,column =0)
 
-currentVal_textLable = tk.Label(currntValFrame,text ="The current value selected is:")
+currentVal_textLable = tk.Label(currntValFrame,text ="The analogWrite() value selected is:")
 currentVal_Lable = tk.Label(currntValFrame,textvariable = curretUserValue)
 currentVal_textLable.grid(row=0, column=0)
 currentVal_Lable.grid(row=0, column=1)
